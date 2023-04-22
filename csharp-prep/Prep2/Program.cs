288e1dd48e601cb89ccf07c58350604ed5bbf076
Luis Rivera <LuisRaulRivera24@gmail.com>
2023-04-22 16:46:20 -0400

    modified Prep1 Program.cs to have variables named according to convention. Also, finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string grade = Console.ReadLine();
+        int gradeOfIndividual = int.Parse(grade);
+
+        string letterGrade = "unknown";
+
+        if (gradeOfIndividual >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradeOfIndividual >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradeOfIndividual >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradeOfIndividual >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (gradeOfIndividual < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}.");
+
+        if (gradeOfIndividual >= 70)
+        {
+            Console.Write("You passed the class!");
+        }
+        else
+        {
+            Console.Write("You did not pass, but can get it next time!");
+        }
+
     }
 }
