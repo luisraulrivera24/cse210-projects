4cd57a8f553c6f236da882ca12f0047c27198fd0
Luis Rivera <LuisRaulRivera24@gmail.com>
2023-04-25 20:52:52 -0400

    I completed my prep 3 assignment. Will have to come back to add comments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
+// Control + K + C will comment code
+// Control + K + U will uncomment code
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1,100);
+
+        int guesses = 0;
+        int guess;
+        
+        do
+        {
+            Console.Write("\nWhat is your guess? ");
+            string number_guess = Console.ReadLine();
+            guess = int.Parse(number_guess);
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+                guesses += 1;
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+                guesses += 1;
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                guesses += 1;
+                Console.WriteLine($"You had a total of {guesses} guess(es).\n");
+            }
+            
+        } while (number != guess);
     }
 }
